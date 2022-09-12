52345c7160717d3102d1d7d05e2f6df2468e6c47
fadilmartias <fadilmartias26@gmail.com>
2022-09-12 16:29:25 +0700

    fix vieww

--- resources/views/layouts/master-without-nav.blade.php
+++ resources/views/layouts/master-without-nav.blade.php
@@ -3,7 +3,7 @@
 
     <head>
         <meta charset="utf-8" />
-        <title> @yield('title') | Dason - Admin & Dashboard Template</title>
+        <title> @yield('title') | Logos - Logistic on Service</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8" />
-    <title> @yield('title') | Dason - Admin & Dashboard Template</title>
+    <title> @yield('title') | Logos - Logistic on Service</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
     <meta content="Themesdesign" name="author" />
