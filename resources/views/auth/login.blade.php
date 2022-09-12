b8f368212e9d7a299e9b38cabe391da510da923b
fadilmartias <fadilmartias26@gmail.com>
2022-09-12 16:22:40 +0700

    fix login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,13 +13,13 @@
                         <div class="d-flex flex-column h-100">
                             <div class="mb-4 mb-md-5 text-center">
                                 <a href="{{ url('/') }}" class="d-block auth-logo">
-                                    <img src="{{ URL::asset('assets/images/logo-sm.svg') }}" alt="" height="28"> <span class="logo-txt">Dason</span>
+                                    <img src="{{ URL::asset('images/logos.jpg') }}" alt="" height="30" style="transform:rotate(90deg); height:150px; width:80px;"> <span class="logo-txt"></span>
                                 </a>
                             </div>
                             <div class="auth-content my-auto">
                                 <div class="text-center">
                                     <h5 class="mb-0">Welcome Back !</h5>
-                                    <p class="text-muted mt-2">Sign in to continue to Dason.</p>
+                                    <p class="text-muted mt-2">Sign in to continue to Logos.</p>
                                 </div>
                                 <form class="mt-4 pt-2" action="{{ route('login') }}" method="POST">
                                     @csrf
@@ -68,40 +68,7 @@
                                     </div>
                                 </form>
 
-                                <div class="mt-4 pt-2 text-center">
-                                    <div class="signin-other-title">
-                                        <h5 class="font-size-14 mb-3 text-muted fw-medium">- Sign in with -</h5>
-                                    </div>
 
-                                    <ul class="list-inline mb-0">
-                                        <li class="list-inline-item">
-                                            <a href="javascript:void()"
-                                                class="social-list-item bg-primary text-white border-primary">
-                                                <i class="mdi mdi-facebook"></i>
-                                            </a>
-                                        </li>
-                                        <li class="list-inline-item">
-                                            <a href="javascript:void()"
-                                                class="social-list-item bg-info text-white border-info">
-                                                <i class="mdi mdi-twitter"></i>
-                                            </a>
-                                        </li>
-                                        <li class="list-inline-item">
-                                            <a href="javascript:void()"
-                                                class="social-list-item bg-danger text-white border-danger">
-                                                <i class="mdi mdi-google"></i>
-                                            </a>
-                                        </li>
-                                    </ul>
-                                </div>
-
-                                <div class="mt-5 text-center">
-                                    <p class="text-muted mb-0">Don't have an account ? <a href="{{ url('register') }}"
-                                            class="text-primary fw-semibold"> Signup now </a> </p>
-                                </div>
-                            </div>
-                            <div class="mt-4 mt-md-5 text-center">
-                                <p class="mb-0">© <script>document.write(new Date().getFullYear())</script> Dason   . Crafted with <i class="mdi mdi-heart text-danger"></i> by Themesdesign</p>
                             </div>
                         </div>
                     </div>
--- storage/framework/views/4a7eb0f831a5d7344d4bf5305d69d2fe1da79357.php
+++ storage/framework/views/4a7eb0f831a5d7344d4bf5305d69d2fe1da79357.php
@@ -1,4 +1,3 @@
-
 <?php $__env->startSection('title'); ?>
 <?php echo app('translator')->get('translation.Login'); ?>
 <?php $__env->stopSection(); ?>
@@ -13,13 +12,13 @@
                         <div class="d-flex flex-column h-100">
                             <div class="mb-4 mb-md-5 text-center">
                                 <a href="<?php echo e(url('/')); ?>" class="d-block auth-logo">
-                                    <img src="<?php echo e(URL::asset('assets/images/logo-sm.svg')); ?>" alt="" height="28"> <span class="logo-txt">Dason</span>
+                                    <img src="<?php echo e(URL::asset('images/logos.jpg')); ?>" alt="" height="30" style="transform:rotate(90deg); height:150px; width:80px;"> <span class="logo-txt"></span>
                                 </a>
                             </div>
                             <div class="auth-content my-auto">
                                 <div class="text-center">
                                     <h5 class="mb-0">Welcome Back !</h5>
-                                    <p class="text-muted mt-2">Sign in to continue to Dason.</p>
+                                    <p class="text-muted mt-2">Sign in to continue to Logos.</p>
                                 </div>
                                 <form class="mt-4 pt-2" action="<?php echo e(route('login')); ?>" method="POST">
                                     <?php echo csrf_field(); ?>
@@ -96,40 +95,7 @@ unset($__errorArgs, $__bag); ?>
                                     </div>
                                 </form>
 
-                                <div class="mt-4 pt-2 text-center">
-                                    <div class="signin-other-title">
-                                        <h5 class="font-size-14 mb-3 text-muted fw-medium">- Sign in with -</h5>
-                                    </div>
 
-                                    <ul class="list-inline mb-0">
-                                        <li class="list-inline-item">
-                                            <a href="javascript:void()"
-                                                class="social-list-item bg-primary text-white border-primary">
-                                                <i class="mdi mdi-facebook"></i>
-                                            </a>
-                                        </li>
-                                        <li class="list-inline-item">
-                                            <a href="javascript:void()"
-                                                class="social-list-item bg-info text-white border-info">
-                                                <i class="mdi mdi-twitter"></i>
-                                            </a>
-                                        </li>
-                                        <li class="list-inline-item">
-                                            <a href="javascript:void()"
-                                                class="social-list-item bg-danger text-white border-danger">
-                                                <i class="mdi mdi-google"></i>
-                                            </a>
-                                        </li>
-                                    </ul>
-                                </div>
-
-                                <div class="mt-5 text-center">
-                                    <p class="text-muted mb-0">Don't have an account ? <a href="<?php echo e(url('register')); ?>"
-                                            class="text-primary fw-semibold"> Signup now </a> </p>
-                                </div>
-                            </div>
-                            <div class="mt-4 mt-md-5 text-center">
-                                <p class="mb-0">© <script>document.write(new Date().getFullYear())</script> Dason   . Crafted with <i class="mdi mdi-heart text-danger"></i> by Themesdesign</p>
                             </div>
                         </div>
                     </div>
